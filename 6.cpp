a3aa20191495e228d9f055f1fcbe695926873e0f
Simay <pilatmd@outlook.com>
2022-04-22 23:06:27 +0300

    f

--- 6.cpp
+++ 6.cpp
@@ -35,6 +35,53 @@ void ex()
 
 int main()
 {
+	setlocale(LC_ALL, "RU");
 
+	queue <int> q;
+
+	cout << "Выберите действие: " << endl
+		<< "1 .Добавить элемент в конец очереди;" << endl
+		<< "2. Добавить элемент в начало очереди;" << endl
+		<< "3. Вывести очередь с конца;" << endl
+		<< "4 .Вывести очередь с начала;" << endl
+		<< "5. Индивидуальный вариант;" << endl
+		<< "6. Выход;" << endl;
+
+	int choose; cin >> choose;
+	while (choose != 6)
+	{
+		switch (choose)
+		{
+		case 1:
+			push_back();
+			break;
+		case 2:
+			push_front();
+			break;
+		case 3:
+			print_back();
+			break;
+		case 4:
+			print_front();
+			break;
+		case 5:
+			ex();
+			break;
+		case 6:
+			return 0;
+		default:
+			cout << "Неправильно выбрано действие..";
+			break;
+		}
+
+		cout << "Выберите действие: " << endl
+			<< "1 .Добавить элемент в конец очереди;" << endl
+			<< "2. Добавить элемент в начало очереди;" << endl
+			<< "3. Вывести очередь с конца;" << endl
+			<< "4 .Вывести очередь с начала;" << endl
+			<< "5. Индивидуальный вариант;" << endl
+			<< "6. Выход;" << endl;
+		cin >> choose;
+	}
 }
 
